1a1982b219ce528815b6aefec19675324451d957
Paweł <pawel.niedziela@gmail.com>
2020-11-29 20:44:33 +0100

    start

---
+++ src/main/java/pl/paweln/dp/DP.java
@@ -0,0 +1,13 @@
+package pl.paweln.dp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DP {
+    private static final Logger logger = LoggerFactory.getLogger(DP.class);
+    public static void main(String[] args) {
+        logger.info("Starting ...");
+
+        logger.info("End.");
+    }
+}
